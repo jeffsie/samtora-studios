8b82efe7d35f9d6620ab14fb135ceae490ad852f
Jeffsie <jeffsie@hotmail.com>
2025-06-19 16:42:39 +1000

    Added port number for local development

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require('http');
+var port = process.env.PORT || 3000;
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
+    var message = 'It works!\nGitHub connection successful.',
         version = 'NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
 });
-server.listen();
+server.listen(port, () => {
+    console.log('Server running at http://localhost:' + port + '/');
+});
